1e3f25b24c283c506ccbf8978fab8e9de7975b17
Andy Quin <andy@kindeo.com>
2018-12-31 01:08:20 +0000

    get the moon blurring nicely

--- src/components/moon/moon.js
+++ src/components/moon/moon.js
@@ -1,11 +1,27 @@
 import React from "react";
+import classnames from "classnames";
+
+import { WithCtx } from "../../context";
 
 import "./moon.scss";
 
-export default function Moon(props) {
+function Moon(props) {
+  const {
+    ctx: { languages }
+  } = props;
+
   return (
     <div className="moon-container">
-      <div className="moon" />
+      <div className="moon">
+        {languages.map(language => (
+          <div
+            className={classnames("half-moon", language)}
+            key={`moon-${language}`}
+          />
+        ))}
+      </div>
     </div>
   );
 }
+
+export default WithCtx(Moon);
