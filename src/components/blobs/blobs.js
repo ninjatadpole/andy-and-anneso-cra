00d4c01e50a5d2f74fce86694e88f30f395dd398
Andy Quin <andy@kindeo.com>
2019-01-13 17:05:12 +0000

    Add blobs component for colour splashes over illustrations

---
+++ src/components/blobs/blobs.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+import sizedArray from "../../utils/sized-array";
+
+import "./blobs.scss";
+
+export default function Blobs(props) {
+  const blobColours = ["blue", "gold", "green", "purple"];
+  const blobs = {};
+
+  blobColours.forEach(colour => {
+    blobs[colour] = ensureArray(props[colour]);
+  });
+
+  return (
+    <div className="blobs">
+      {blobColours.map(colour => {
+        if (blobs[colour]) {
+          return blobs[colour].map(version => {
+            return (
+              <div
+                className={`blob blob-${version} blob-${version}-${colour}`}
+                key={`blob-${version}-${colour}`}
+              />
+            );
+          });
+        }
+
+        return null;
+      })}
+      {props.children}
+    </div>
+  );
+}
+
+function ensureArray(item) {
+  if (Array.isArray(item) || !item) {
+    return item;
+  } else {
+    return [item];
+  }
+}
