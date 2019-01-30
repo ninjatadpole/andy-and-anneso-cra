28f53737e8f6c7df23855940fc105a88b6707c97
Andy Quin <andy@kindeo.com>
2019-01-30 21:47:47 +0000

    get rid of all the fairy light stuff

--- src/components/landscape/landscape.js
+++ src/components/landscape/landscape.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import Blobs from "../blobs";
-import FairyLight from "../fairy-light";
 import Leaves from "../leaves";
 import Tree from "../tree";
 
@@ -16,33 +15,9 @@ export default function Landscape(props) {
       <div className="trees mirror">
         <Tree version={1}>
           <Leaves className="leaves-1" leaves={[1, 2, 3]} />
-          <div className="fairy-lights fairy-lights-1">
-            <FairyLight className="fairy-light-1" gold={[1, 2, 3]} />
-            <FairyLight className="fairy-light-2" purple={[1, 2, 3]} />
-            <FairyLight className="fairy-light-3" gold={[1, 2, 3]} />
-            <FairyLight className="fairy-light-4" purple={[1, 2, 3]} />
-            <FairyLight className="fairy-light-5" gold={[1, 2, 3]} />
-            <FairyLight className="fairy-light-6" purple={[1, 2, 3]} />
-            <FairyLight className="fairy-light-7" gold={[1, 2, 3]} />
-            <FairyLight className="fairy-light-8" purple={[1, 2, 3]} />
-            <FairyLight className="fairy-light-9" gold={[1, 2, 3]} />
-            <FairyLight className="fairy-light-10" purple={[1, 2, 3]} />
-          </div>
         </Tree>
         <Tree version={2}>
           <Leaves className="leaves-2" leaves={[1, 2, 3]} />
-          <div className="fairy-lights fairy-lights-2">
-            <FairyLight className="fairy-light-1" gold={[1, 2, 3]} />
-            <FairyLight className="fairy-light-2" purple={[1, 2, 3]} />
-            <FairyLight className="fairy-light-3" gold={[1, 2, 3]} />
-            <FairyLight className="fairy-light-4" purple={[1, 2, 3]} />
-            <FairyLight className="fairy-light-5" gold={[1, 2, 3]} />
-            <FairyLight className="fairy-light-6" purple={[1, 2, 3]} />
-            <FairyLight className="fairy-light-7" gold={[1, 2, 3]} />
-            <FairyLight className="fairy-light-8" purple={[1, 2, 3]} />
-            <FairyLight className="fairy-light-9" gold={[1, 2, 3]} />
-            <FairyLight className="fairy-light-10" purple={[1, 2, 3]} />
-          </div>
         </Tree>
       </div>
       {props.children}
--- src/components/tree/tree.js
+++ src/components/tree/tree.js
@@ -1,8 +1,6 @@
 import React from "react";
 import classnames from "classnames";
 
-import Blobs from "../blobs";
-
 import "./tree.scss";
 
 export default function Tree(props) {
