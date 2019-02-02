b7f55c2d82b5d73c79b268da69ac1d2251af91a9
Andy Quin <andy@kindeo.com>
2019-02-02 22:02:50 +0000

    Add drop cap blob. Add another blob to moon and place blobs behind illustration.

--- src/components/moon/moon.js
+++ src/components/moon/moon.js
@@ -14,13 +14,13 @@ function Moon(props) {
   return (
     <div className="moon-container">
       <div className="moon">
+        <Blobs gold={[1, 2, 3]} />
         {languages.map(language => (
           <div
             className={classnames("half-moon", language)}
             key={`moon-${language}`}
           />
         ))}
-        <Blobs gold={[3, 2]} />
       </div>
     </div>
   );
