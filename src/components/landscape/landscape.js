67778770d1125634a7cf2f3c92e49b6590ba06b5
Andy Quin <andy@kindeo.com>
2019-02-02 20:36:35 +0000

    Add fairy lights to trees

--- src/components/landscape/landscape.js
+++ src/components/landscape/landscape.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Blobs from "../blobs";
+import FairyLight from "../fairy-light";
 import Leaves from "../leaves";
 import Tree from "../tree";
 
@@ -15,12 +16,35 @@ export default function Landscape(props) {
       <div className="trees mirror">
         <Tree version={1} className="tree-1">
           <Leaves className="leaves-1" leaves={[1, 2, 3]} />
+          <div className="fairy-lights-1">
+            <FairyLight className="light-1" gold={[1, 2, 3]} />
+            <FairyLight className="light-2" gold={[1, 2, 3]} />
+            <FairyLight className="light-3" gold={[1, 2, 3]} />
+            <FairyLight className="light-4" gold={[1, 2, 3]} />
+            <FairyLight className="light-5" gold={[1, 2, 3]} />
+            <FairyLight className="light-6" gold={[1, 2, 3]} />
+            <FairyLight className="light-7" gold={[1, 2, 3]} />
+            <FairyLight className="light-8" gold={[1, 2, 3]} />
+            <FairyLight className="light-9" gold={[1, 2, 3]} />
+            <FairyLight className="light-10" gold={[1, 2, 3]} />
+          </div>
         </Tree>
         <Tree version={2} className="tree-2">
           <Leaves className="leaves-2" leaves={[1, 2, 3]} />
+          <div className="fairy-lights-2">
+            <FairyLight className="light-1" gold={[1, 2, 3]} />
+            <FairyLight className="light-2" gold={[1, 2, 3]} />
+            <FairyLight className="light-3" gold={[1, 2, 3]} />
+            <FairyLight className="light-4" gold={[1, 2, 3]} />
+            <FairyLight className="light-5" gold={[1, 2, 3]} />
+          </div>
         </Tree>
         <Tree version={2} className="tree-3">
           <Leaves className="leaves-3" leaves={[1, 2, 3]} />
+          <div className="fairy-lights-3">
+            <FairyLight className="light-1" gold={[1, 2, 3]} />
+            <FairyLight className="light-2" gold={[1, 2, 3]} />
+          </div>
         </Tree>
       </div>
       {props.children}
