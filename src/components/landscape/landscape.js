76aa2f7367c00c68c7b4a660faa4a9422bbf2dcd
Andy Quin <andy@kindeo.com>
2019-02-10 11:46:01 +0000

    Update style of fairy lights, change to purple, broaden spread of the moon

--- src/components/landscape/landscape.js
+++ src/components/landscape/landscape.js
@@ -17,34 +17,34 @@ export default function Landscape(props) {
         <Tree version={1} className="tree-1">
           <Leaves className="leaves-1" leaves={[1, 2, 3]} />
           <div className="fairy-lights-1">
-            <FairyLight className="light-1" gold={[1, 2, 3]} />
-            <FairyLight className="light-2" gold={[1, 2, 3]} />
-            <FairyLight className="light-3" gold={[1, 2, 3]} />
-            <FairyLight className="light-4" gold={[1, 2, 3]} />
-            <FairyLight className="light-5" gold={[1, 2, 3]} />
-            <FairyLight className="light-6" gold={[1, 2, 3]} />
-            <FairyLight className="light-7" gold={[1, 2, 3]} />
-            <FairyLight className="light-8" gold={[1, 2, 3]} />
-            <FairyLight className="light-9" gold={[1, 2, 3]} />
-            <FairyLight className="light-10" gold={[1, 2, 3]} />
+            <FairyLight className="light-1" purple={[1, 2, 3]} />
+            <FairyLight className="light-2" purple={[1, 2, 3]} />
+            <FairyLight className="light-3" purple={[1, 2, 3]} />
+            <FairyLight className="light-4" purple={[1, 2, 3]} />
+            <FairyLight className="light-5" purple={[1, 2, 3]} />
+            <FairyLight className="light-6" purple={[1, 2, 3]} />
+            <FairyLight className="light-7" purple={[1, 2, 3]} />
+            <FairyLight className="light-8" purple={[1, 2, 3]} />
+            <FairyLight className="light-9" purple={[1, 2, 3]} />
+            <FairyLight className="light-10" purple={[1, 2, 3]} />
           </div>
         </Tree>
         <Tree version={2} className="tree-2">
           <Leaves className="leaves-2" leaves={[1, 2, 3]} />
           <div className="fairy-lights-2">
-            <FairyLight className="light-1" gold={[1, 2, 3]} />
-            <FairyLight className="light-2" gold={[1, 2, 3]} />
-            <FairyLight className="light-3" gold={[1, 2, 3]} />
-            <FairyLight className="light-4" gold={[1, 2, 3]} />
-            <FairyLight className="light-5" gold={[1, 2, 3]} />
+            <FairyLight className="light-1" purple={[1, 2, 3]} />
+            <FairyLight className="light-2" purple={[1, 2, 3]} />
+            <FairyLight className="light-3" purple={[1, 2, 3]} />
+            <FairyLight className="light-4" purple={[1, 2, 3]} />
+            <FairyLight className="light-5" purple={[1, 2, 3]} />
           </div>
         </Tree>
         <Tree version={2} className="tree-3">
           <Leaves className="leaves-3" leaves={[1, 2, 3]} />
           <div className="fairy-lights-3">
-            <FairyLight className="light-1" gold={[1, 2, 3]} />
-            <FairyLight className="light-2" gold={[1, 2, 3]} />
-            <FairyLight className="light-3" gold={[1, 2, 3]} />
+            <FairyLight className="light-1" purple={[1, 2, 3]} />
+            <FairyLight className="light-2" purple={[1, 2, 3]} />
+            <FairyLight className="light-3" purple={[1, 2, 3]} />
           </div>
         </Tree>
       </div>
